dd60d0a965472df19ba19918835b1b230059c48e
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-06-16 21:47:19 +0500

    yar bss last 4

--- webui.py
+++ webui.py
@@ -175,6 +175,11 @@ with shared.gradio_root:
                                              elem_id='negative_prompt',
                                              value=modules.config.default_prompt_negative)
 
+                overwrite_step = gr.Slider(label='Forced Overwrite of Sampling Step',
+                                           minimum=-1, maximum=200, step=1,
+                                           value=modules.config.default_overwrite_step,
+                                           info='Set as -1 to disable. For developer debugging.')
+
             with gr.Tab(label='Styles'):
                 style_sorter.try_load_sorted_styles(
                     style_names=legal_style_names,
